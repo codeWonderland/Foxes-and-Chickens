735e5f87a26f28acab5a935ed7fb78ff293a82db
codeWonderland <easter@champlain.edu>
2019-02-05 14:04:56 -0500

    Init Commit

---
+++ Foxes and Chickens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foxes and Chickens
+//
+//  Created by Easter, Alice on 2/5/19.
+//  Copyright © 2019 Easter, Alice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
