a032e0153759787cf73ae7efa5d395070e2f9715
codeWonderland <easter@champlain.edu>
2019-02-07 15:14:06 -0500

    OUTLETS

--- Foxes and Chickens/ViewController.swift
+++ Foxes and Chickens/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chickenLOne: UIButton!
+    @IBOutlet weak var chickenLTwo: UIButton!
+    @IBOutlet weak var chickenLThree: UIButton!
+    @IBOutlet weak var foxLOne: UIButton!
+    @IBOutlet weak var foxLTwo: UIButton!
+    @IBOutlet weak var foxLThree: UIButton!
+    @IBOutlet weak var boatLPassengerL: UIButton!
+    @IBOutlet weak var boatLPassengerR: UIButton!
+    @IBOutlet weak var boatL: UIImageView!
+    @IBOutlet weak var chickenROne: UIButton!
+    @IBOutlet weak var chickenRTwo: UIButton!
+    @IBOutlet weak var chickenRThree: UIButton!
+    @IBOutlet weak var foxROne: UIButton!
+    @IBOutlet weak var foxRTwo: UIButton!
+    @IBOutlet weak var foxRThree: UIButton!
+    @IBOutlet weak var boatRPassengerL: UIButton!
+    @IBOutlet weak var boatRPassengerR: UIButton!
+    @IBOutlet weak var boatR: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        foxROne.isHidden = true
+        foxRTwo.isHidden = true
+        foxRThree.isHidden = true
+        
     }
-
-
+    
+    @IBAction func cross(_ sender: UIButton) {
+    
+    }
 }
 
